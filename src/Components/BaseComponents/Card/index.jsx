ce2993d2548dcc5bc0e63fd785603da8db55d123
kumesh1997 <tharindukumesh09@gmail.com>
2022-05-19 16:36:04 +0530

    Toggle Button fixed

--- src/Components/BaseComponents/Card/index.jsx
+++ src/Components/BaseComponents/Card/index.jsx
@@ -7,10 +7,15 @@ import '../Card/card.css';
 function Card(card){
     console.log(card.info);
     let img_path="https://image.tmdb.org/t/p/w500";
+
+
     return(
         <div id="card" className=" w-full  h-70  bg-blue-200 shadow-lg duration-500 box-border border-2 ">
+
+            {/* Movie Banar */}
             <img src={img_path+card.info.poster_path} alt="Image" className=" w-full h-70  max-h-70" />
-    
+
+            {/* Card Body */}
             <div id="card-body" className=" flex flex-col justify-center align-middle">
                 <div className="box w-full p-3 flex align-middle justify-around">
                     <MovieTitle title={card.info.title}/>
@@ -22,13 +27,13 @@ function Card(card){
                         <p className="">Original Language<span className=" font-extrabold"> {card.info.original_language}</span></p>
                 </div>
 
+                {/* Overview */}
                 {/* <div className="overview p-3">
                     <h1 className=" font-bold ">Overview</h1><br/>
                     <p className=" text-justify">{card.info.overview}</p>
                 </div>  */}
                   
             </div>
-
         </div>
     );
 }
